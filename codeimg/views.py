17bdf43f9fcf804b07489b00ac55b6c3644fdf62
mikhailborodin <it@mikhailborodin.com>
2023-09-08 17:44:35 -0300

    add all styles

--- codeimg/views.py
+++ codeimg/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import FormView
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import Python3Lexer
+from pygments.styles import get_all_styles
+
 from .forms import CodeForm
 
 
@@ -8,13 +13,18 @@ class CodeFormView(FormView):
     success_url = "/"
 
     def get_context_data(self, **kwargs):
+        formatter = HtmlFormatter(style=self.request.session.get("style", None))
         context = super(CodeFormView, self).get_context_data(**kwargs)
         form = self.get_form()
         if default_code := self.request.session.get("default_code", False):
             form.fields['code'].initial = default_code
+            context["highlighted_code"] = highlight(default_code, Python3Lexer(), formatter)
         else:
             form.fields['code'].initial = "Type your code here"
         context["form"] = form
+        context["style_definitions"] = formatter.get_style_defs()
+        context["all_styles"] = list(get_all_styles())
+        context["style_bg_color"] = formatter.style.background_color
         return context
 
     def get_form_kwargs(self):
@@ -22,4 +32,6 @@ class CodeFormView(FormView):
         if self.request.method == "POST":
             if code := kwargs["data"].get("code"):
                 self.request.session["default_code"] = code
+            if style := kwargs["data"].get("style"):
+                self.request.session["style"] = style
         return kwargs
