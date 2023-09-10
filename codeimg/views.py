d9a1ccf578ac3a6d307a41051fb6a0402173484b
mikhailborodin <it@mikhailborodin.com>
2023-09-10 16:05:37 -0300

    work with sessions

--- codeimg/views.py
+++ codeimg/views.py
@@ -10,16 +10,18 @@ class CodeFormView(FormView):
     def get_context_data(self, **kwargs):
         context = super(CodeFormView, self).get_context_data(**kwargs)
         form = self.get_form()
-        if default_code := self.request.session.get("default_code", False):
+        if default_code := self.request.session.get("code", False):
             form.fields['code'].initial = default_code
         else:
             form.fields['code'].initial = "Type your code here"
         context["form"] = form
         return context
 
-    def get_form_kwargs(self):
-        kwargs = super(CodeFormView, self).get_form_kwargs()
-        if self.request.method == "POST":
-            if code := kwargs["data"].get("code"):
-                self.request.session["default_code"] = code
-        return kwargs
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            code = form.data["code"]
+            self.request.session["code"] = code
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
